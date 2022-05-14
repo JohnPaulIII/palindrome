20b25a1c146df81eecc820f29046555d8c565350
John Paul III <jcpickel2@gmail.com>
2022-05-14 15:01:41 -0400

    Initialize repository

---
+++ lib/JohnPaulIII_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "JohnPaulIII_palindrome/version"
+
+module JohnPaulIIIPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
